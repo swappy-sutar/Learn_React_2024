2cfc69d38d93ef3646f8ed7b85277331f1977a81
swapnil <sutarswapnil322@gmail.com>
2024-06-17 10:41:46 +0530

    learn useEffect

--- 04_UseEffect/src/App.jsx
+++ 04_UseEffect/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState("");
+  const [count1, setCount1] = useState(1);
+  const [count2, setCount2] = useState(1);
+
+  useEffect(() => {
+    console.log("useEffect called");
+  }, []);
+
+  const increment1 = () => {
+    setCount1(count1 + 1);
+  };
+
+  const increment2 = () => {
+    setCount2(count2 + 1);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://randomuser.me/api/");
+      const data = await res.json();
+      const jsonString = JSON.stringify(data);
+      setUser(jsonString);
+    };
+
+    fetchData();
+  }, [count1, count2]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>1: {count1}</p>
+      <button onClick={increment1}>increment </button>
+      <br />
+      <p>2: {count2} </p>
+      <button onClick={increment2}>increment </button>
+      <br />
+      {user}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
